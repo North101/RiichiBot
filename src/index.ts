bedf699e2a1b8bf18f434b991169b4b686673478
Alexander Wilde <alexander@salesseek.com>
2020-06-28 01:37:30 +0100

    Add and update yaku translations

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import * as Discord from 'discord.js';
-import Riichi from 'riichi';
 import dotenv from 'dotenv';
+import Riichi from 'riichi';
 
 dotenv.config();
 
@@ -452,11 +452,12 @@ class RiichiBot {
         '場風北': 'North Round Wind (Pei Bakaze)',
         '嶺上開花': 'Win Off The Replacement Tile (Rinshan Kaihou)',
         '搶槓': 'Robbing The Kan (Chankan)',
-        '海底撈月': 'Last Pick (Haitei Raoyue)',
-        '河底撈魚': 'Last Discard (Houtei Raoyui)',
-        'ダブルリーチ': 'Double Riichi (Double Riichi)',
+        '海底摸月': 'Win By Last Draw (Haitei Raoyue)',
+        '河底撈魚': 'Win By Last Discard (Houtei Raoyui)',
+        'ダブル立直': 'Double Riichi (Double Riichi)',
         '七対子': 'Seven Pairs (Chiitoitsu)',
-        '全帯幺九': 'Mixed Terminal Hand (Chanta)',
+        '大七星': 'Great Seven Stars (Dai Shichisei)',
+        '混全帯么九': 'Mixed Terminal Hand (Chanta)',
         '三色同順': 'Triple Sequence (Sanshoku Doujun)',
         '一気通貫': 'Straight (Ittsu (Ikkitsuukan))',
         '対々': 'All Triplets (Toitoi)',
@@ -465,7 +466,7 @@ class RiichiBot {
         '三色同刻': 'Triple Triplets (Sanshoku Doukou)',
         '三槓子': 'Three Kans (Sankantsu)',
         '小三元': 'Little Three Dragons (Shousangen)',
-        '混老頭': 'Pure Terminal Hand (Honroutou)',
+        '純全帯么九': 'Pure Terminal Hand (Honroutou)',
         '二盃口': 'Double Double Sequence (Ryanpeikou)',
         '混一色': 'Mixed Flush (Honitsu)',
         '純全帯么': 'Mixed Ends Hand (Junchan)',
@@ -473,8 +474,9 @@ class RiichiBot {
         '天和': 'Miracle Start (Dealer) (Tenhou)',
         '地和': 'Miracle Start (Non-Dealer) (Chiihou)',
         '国士無双': 'Thirteen Orphans (Kokushi Musou)',
-        '国士無双十三面待ち': 'Thirteen Orphans 13 Sided Wait (Kokushi Musou Jū San Menmachi)',
-        '九連宝燈': 'Nine Lanterns (Chuuren Poutou)',
+        '国士無双十三面待ち': 'Thirteen Orphans 13 Closed Wait (Kokushi Musou Juusan Menmachi)',
+        '九蓮宝燈': 'Nine Lanterns (Chuuren Poutou)',
+        '純正九蓮宝燈': 'Pure Nine Lanterns  (Junsei Chuuren Poutou)',
         '四暗刻': 'Four Concealed Triples (Suuankou)',
         '四暗刻単騎待ち': 'Four Concealed Triples Pair Wait (Suuankou Tanki)',
         '大三元': 'Big Three Dragons (Daisangen)',
